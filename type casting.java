6d0d37092dc1cd4e8b6beb3a564668f079825009
prashanthshakthi23-hue <prashanthshakthi23@gmail.com>
2026-09-17 08:18:34 +0530

    Add files via upload

---
+++ type casting.java
@@ -0,0 +1,9 @@
+class TypeCasting {
+    public static void main(String[] args) {
+        double a = 10.5;
+        int b = (int) a;
+
+        System.out.println("Double value: " + a);
+        System.out.println("Integer value: " + b);
+    }
+}
