4617341bc56117af62fbfe46ab77b20e5571abe7
prashanthshakthi23-hue <prashanthshakthi23@gmail.com>
2026-09-17 08:19:11 +0530

    Add files via upload

---
+++ classsimpleif.java
@@ -0,0 +1,9 @@
+class SimpleIf {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote");
+        }
+    }
+}
